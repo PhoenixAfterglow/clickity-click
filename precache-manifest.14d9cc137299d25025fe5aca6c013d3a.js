7b9e2abe814d4bd03553658510379e35d824a89a
Johnny Maravelis <johnny@phoenixafterglow.com>
2019-11-10 12:19:02 -0500

    Updates

---
+++ precache-manifest.14d9cc137299d25025fe5aca6c013d3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ebdfe814909568c6ebfbf3c91c6613",
+    "url": "/clickity-click/index.html"
+  },
+  {
+    "revision": "3f6b74978431ba30c83d",
+    "url": "/clickity-click/static/css/main.66b45759.chunk.css"
+  },
+  {
+    "revision": "3e768e4670af23eaa186",
+    "url": "/clickity-click/static/js/2.755cad25.chunk.js"
+  },
+  {
+    "revision": "3f6b74978431ba30c83d",
+    "url": "/clickity-click/static/js/main.be8c3fbe.chunk.js"
+  },
+  {
+    "revision": "1c8cb628a8c54dd0956e",
+    "url": "/clickity-click/static/js/runtime-main.8a176890.js"
+  },
+  {
+    "revision": "441861dd4e8ed4a8d630de82d287fc64",
+    "url": "/clickity-click/static/media/pokeball.441861dd.png"
+  }
+]);
